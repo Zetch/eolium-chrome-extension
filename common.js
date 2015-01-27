ffb0dda8206727ff48707b127d35193598f9c7a4
Nauzet Hernandez <nauzet@horizon.tng.iac.es>
2015-01-27 17:10:36 +0000

    Code restructuration

--- common.js
+++
@@ -1,9 +0,0 @@
-
-function sanitizeText(text) {
-  return text.toLowerCase()
-    .replace(/[àáâãä]/g, "a")
-    .replace(/[èéêẽë]/g, "e")
-    .replace(/[ìíîĩï]/g, "i")
-    .replace(/[òóôõö]/g, "o")
-    .replace(/[ùúûũü]/g, "u");
-}
