670264b46d87b86c0dd30e65dcba84351bbf0f25
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2016-02-26 14:50:38 +0000

    Old code removed, now filters keep state between requests

--- js/forums.js
+++ js/forums.js
@@ -1,216 +1,220 @@
-
-var fLink  = window.location.pathname;
-var fTitle = Array.prototype.reduce.call(
-  document.querySelectorAll('h2.breadcrumbs a span'), 
-  function(p, n) {
-    if (p.innerHTML === undefined) {
-      return p + " / " + n.innerHTML;
-    }
-    return p.innerHTML + " / " + n.innerHTML;
-  }
-);
-
-
-function setFilter(keyword, rows) {
-  // Convert text before search
-  var keywd = sanitizeText(keyword);
-
-  // Check rows
-  Array.prototype.forEach.call(rows, function(row) {
-    var threadLink = row.querySelector('dl > dt > a.topictitle');
-    // Check link exists
-    if (threadLink) {
-      var title = sanitizeText(threadLink.getAttribute('title'));
-      // Check link text doesn't contain keyword, then hide row
-      if (title.search(keywd) < 0) {
-        // Avoid to add the same class twice or more
-        if (!row.classList.contains('noDisplay')) {
-          row.classList.add('noDisplay');
-        }
-      // If keyword exists but row was previously hidden, show it
-      } else if (row.classList.contains('noDisplay')) {
-        row.classList.remove('noDisplay');
-      // Keyword exists and it's not hidden
-      } else {
-        // OK
-      }
-    }
-  });
-}
-
-
-function loadIgnored(rows, manager, callback) {
-  var mToggle = document.createElement("a");
-  mToggle.setAttribute('href', '#');
-  mToggle.setAttribute('class', 'ignore-thread');
-  mToggle.innerHTML = '&times;';
-
-  Array.prototype.forEach.call(rows, function(row) {
-    // Add toggles
-    var threadlink = row.querySelector('dl > dt > a.topictitle');
-    var toggle = mToggle.cloneNode(true);
-
-    // Add event, toggle thread as ignored
-    toggle.addEventListener('click', function(e) {
-      e.preventDefault();
-      // Parse info
-      var tLink  = event.target.previousElementSibling.getAttribute('href');
-      var tTitle = event.target.previousElementSibling.getAttribute('title');
-
-      manager.toggle(fTitle, fLink, tTitle, tLink);
-      manager.save(function() {
-        e.target.classList.toggle('ignored');
-        console.info('Thread added');
-
-        // Toggle visibility if filter is active
-        if (!document.getElementById('buttonIgnored').classList.contains('active') &&
-            e.target.classList.contains('ignored')) {
-          e.target.parentElement.parentElement.parentElement.classList.toggle('hideIgnored');
-        }
-      });
-    });
-
-    // Check if it's ignored
-    if (manager.isThreadIgnored(fLink, threadlink.getAttribute('href'))) {
-      toggle.classList.add('ignored');
-    }
-
-    threadlink.parentElement.insertBefore(toggle, threadlink.nextElementSibling);
-  });
-
-  // Run callback
-  callback();
-}
-
-
-function toggleVisibility(toggle, hidden, rows) {
-  // Check given toggle name
-  var selector, className;
-  if (toggle === 'read') {
-    selector  = 'a:not(.rowunre)';
-    className = 'hideRead';
-  } else if (toggle === 'closed') {
-    selector = 'a.rowlock';
-    className = 'hideClosed';
-  } else if (toggle === 'archived') {
-    selector = 'a.rowarch';
-    className = 'hideArchived';
-  } else if (toggle === 'ignored') {
-    selector = 'dt a.ignored';
-    className = 'hideIgnored';
-  } else {
-    // Do nothing
-    return;
-  }
-  // Toggle class on desired rows
-  Array.prototype.forEach.call(rows, function(row) {
-    if (row.querySelector('dl > ' + selector)) {
-      row.classList.toggle(className);
-    }
-  });
-}
-
-
-// If it's a forum and it can be filtered (exclude forums without topics)
-if (document.querySelector('#content-wrap #forum-wrap') &&
-    document.querySelector('#forum-wrap .topic-actions')) {
-
-  // Bind elements
-  var form = document.querySelector('#forum-wrap .topic-actions #forum-search');
-  var rows = document.querySelectorAll('#forum-wrap .forumbg:not(.announcement) .topics .row');
-  
-  // Fix display buttons when full width is disabled
-  var markAsRead = document.querySelector('#forum-wrap .topic-actions .pagination a[accesskey="m"]')
-  if (markAsRead) {
-    markAsRead.textContent = 'Marcar como leídos';
-  }
-
-  // Create new elements for filtering
-  var inputFilter = document.createElement('input');
-  inputFilter.setAttribute('type',        'text');
-  inputFilter.setAttribute('id',          'inputFilter');
-  inputFilter.setAttribute('placeholder', 'Filtrar hilos...');
-
-  var buttonRead = document.createElement('button');
-  buttonRead.setAttribute('id',    'buttonRead');
-  buttonRead.setAttribute('title', 'Leídos');
-  buttonRead.setAttribute('class', 'active');
-
-  var buttonClosed = document.createElement('button');
-  buttonClosed.setAttribute('id',    'buttonClosed');
-  buttonClosed.setAttribute('title', 'Cerrados');
-  buttonClosed.setAttribute('class', 'active');
-
-  var buttonArchived = document.createElement('button');
-  buttonArchived.setAttribute('id',    'buttonArchived');
-  buttonArchived.setAttribute('title', 'Archivados');
-  buttonArchived.setAttribute('class', 'active');
-
-  var buttonIgnored = document.createElement('button');
-  buttonIgnored.setAttribute('id',    'buttonIgnored');
-  buttonIgnored.setAttribute('title', 'Ignorados');
-  buttonIgnored.setAttribute('class', 'active');
-
-
-  // Add input filter event, append to page
-  inputFilter.addEventListener('input', function(e) {
-    setFilter(e.target.value, rows);
-  });
-
-  form.appendChild(inputFilter);
+(function(){
+  'use strict';
 
   // Load preferences for toggles
   chrome.storage.local.get({
-    'eolium_forums_hideRead':      false,
-    'eolium_forums_hideClosed':    false,
-    'eolium_forums_hideArchived':  false,
-    'eolium_forums_hideIgnored':   false,
-    'eolium_forums_ignoredThreads': {}
+    'eolium_forums_hideFilter':     '',
+    'eolium_forums_hideRead':       false,
+    'eolium_forums_hideClosed':     false,
+    'eolium_forums_hideArchived':   false,
+    'eolium_forums_hideIgnored':    false,
+    'eolium_forums_ignoredThreads': []
 
-  }, function(items) {
-    var preferences = items;
+  }, (preferences) => {
 
-    if (preferences['eolium_forums_hideRead']) {
-      toggleVisibility('read', buttonRead.classList.toggle('active'), rows);
-    }
-    buttonRead.addEventListener('click', function(e) {
-      e.preventDefault();
-      toggleVisibility('read', e.target.classList.toggle('active'), rows);
-    });
+    // Get topic list
+    const topics = Array.prototype.slice.call(document.querySelectorAll('.row.topic:not(.announcement):not(.wikir)'));
 
-    if (preferences['eolium_forums_hideClosed']) {
-      toggleVisibility('closed', buttonClosed.classList.toggle('active'), rows);
-    }
-    buttonClosed.addEventListener('click', function(e) {
-      e.preventDefault();
-      toggleVisibility('closed', e.target.classList.toggle('active'), rows);
-    });
+    // Load ignored topics and set handlers
+    ((ignored) => {
 
-    if (preferences['eolium_forums_hideArchived']) {
-      toggleVisibility('archived', buttonArchived.classList.toggle('active'), rows);
-    }
-    buttonArchived.addEventListener('click', function(e) {
-      e.preventDefault();
-      toggleVisibility('archived', e.target.classList.toggle('active'), rows);
-    });
+      // Parse ID from given path
+      const regexId = /(\d+(_s\d+)?)?$/;
+      const getId = (path) => parseInt(regexId.exec(path)[0].split('_')[0]);
 
-    // Load ignored threads
-    var manager = new IgnoredManager(preferences['eolium_forums_ignoredThreads']);
+      // Get subforum data
+      const forumId = getId(window.location.pathname);
+      const forumName = Array.prototype
+        .slice.call(document.querySelectorAll('#breadcrumbs a'))
+        .map(n => n.textContent.trim())
+        .join(' > ');
+      ;
 
-    loadIgnored(rows, manager, function() {
-      if (preferences['eolium_forums_hideIgnored']) {
-        toggleVisibility('ignored', buttonIgnored.classList.toggle('active'), rows);
+      // Get subforum ignored topics
+      const ignoredTopics = ignored.filter(topic => topic.forumId === forumId);
+
+      function toggleIgnored(event) {
+        event.preventDefault();
+        const isIgnored = event
+          .target     // .toggle
+          .parentNode // .title
+          .parentNode // .row
+          .parentNode // .col
+          .parentNode // .row
+          .parentNode // .col
+          .parentNode.firstElementChild.classList.toggle('rowignore');
+        const link = event.target.previousElementSibling;
+        const id = getId(link.getAttribute('href'))
+        const title = link.textContent;
+
+        if (isIgnored) {
+          ignored.push({
+            id:        id,
+            title:     title.trim(),
+            url:       link.getAttribute('href'),
+            forumId:   forumId,
+            forumName: forumName
+          });
+        } else {
+          ignored = ignored.filter(topic => topic.id !== id);
+        }
+
+        // Save
+        chrome.storage.local.set({ 'eolium_forums_ignoredThreads': ignored });
       }
-      buttonIgnored.addEventListener('click', function(e) {
-        e.preventDefault();
-        toggleVisibility('ignored', e.target.classList.toggle('active'), rows);
-      });
-    });
 
-    // Append to page
-    form.appendChild(buttonRead);
-    form.appendChild(buttonClosed);
-    form.appendChild(buttonArchived);
-    form.appendChild(buttonIgnored);
+      topics.forEach((topic) => {
+        const title = topic.querySelector('.title a');
+        const id = getId(title.getAttribute('href'));
+
+        if (ignoredTopics.filter(i => i.id === id).length) {
+          topic.firstElementChild.classList.add('rowignore');
+        }
+
+        const toggle = document.createElement('a');
+        toggle.classList.add('toggle');
+        toggle.textContent = '×';
+        toggle.setAttribute('href', '#')
+        toggle.addEventListener('click', toggleIgnored);
+        title.parentNode.appendChild(toggle);
+      });
+
+    })(preferences['eolium_forums_ignoredThreads']);
+
+
+    // Sanitize text before filtering
+    function sanitizeText(text) {
+      return text.toLowerCase()
+        .replace(/[àáâãä]/g, "a")
+        .replace(/[èéêẽë]/g, "e")
+        .replace(/[ìíîĩï]/g, "i")
+        .replace(/[òóôõö]/g, "o")
+        .replace(/[ùúûũü]/g, "u");
+    }
+
+    // Set filter by pattern
+    function setFilter(event) {
+      const pattern = sanitizeText(event.target.value);
+
+      topics.forEach((topic) => {
+        const title = sanitizeText(topic.querySelector('.title a').getAttribute('title'));
+        // Check link text doesn't contain keyword, then hide row
+        if (title.search(pattern) < 0) {
+          // Avoid to add the same class twice or more
+          if (!topic.classList.contains('hide')) {
+            topic.classList.add('hide');
+          }
+        // If keyword exists but topic was previously hidden, show it
+        } else if (topic.classList.contains('hide')) {
+          topic.classList.remove('hide');
+        }
+      });
+    }
+
+    // Set filter by state
+    function toggleVisibility(event) {
+      let selector, className, optionName;
+
+      switch (event.target.id) {
+        case 'toggle-read':
+          selector  = '.topic > a.topicbtn:not(.rowunre):not(.rowmove)';
+          className = 'hide-read';
+          optionName = 'eolium_forums_hideRead';
+          break;
+
+        case 'toggle-closed':
+          selector = '.topic > a.topicbtn.rowlock';
+          className = 'hide-closed';
+          optionName = 'eolium_forums_hideClosed';
+          break;
+
+        case 'toggle-archived':
+          selector = '.topic > a.topicbtn.rowarch';
+          className = 'hide-archived';
+          optionName = 'eolium_forums_hideArchived';
+          break;
+
+        case 'toggle-ignored':
+          selector = '.topic > a.topicbtn.rowignore';
+          className = 'hide-ignored';
+          optionName = 'eolium_forums_hideIgnored';
+          break;
+
+        default:
+          return;
+      }
+
+      // Change button status and save as default
+      const state = event.target.classList.toggle('active');
+      chrome.storage.local.set({ [optionName]: state });
+
+      // Toggle class on desired rows
+      Array.prototype
+        .slice.call(document.querySelectorAll(selector))
+        .forEach(link => link.parentNode.classList.toggle(className))
+      ;
+    }
+
+    // Create filter form
+    const filterForm = (() => {
+      const form = document.createElement('form');
+
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('placeholder', 'Filtrar hilos...');
+        input.addEventListener('input', setFilter);
+        input.addEventListener('keydown', (e) => {
+          // Clean filter with 'ESC' key
+          if (e.keyCode === 27) {
+            input.value = '';
+            setFilter(e);
+          }
+        });
+
+        const toggleRead = document.createElement('input');
+        toggleRead.id = 'toggle-read';
+        toggleRead.setAttribute('type', 'button');
+        toggleRead.classList.add('toggle');
+        toggleRead.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_forums_hideRead']) toggleRead.click();
+
+        const toggleClosed = document.createElement('input');
+        toggleClosed.id = 'toggle-closed';
+        toggleClosed.setAttribute('type', 'button');
+        toggleClosed.classList.add('toggle');
+        toggleClosed.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_forums_hideClosed']) toggleClosed.click();
+
+        const toggleArchived = document.createElement('input');
+        toggleArchived.id = 'toggle-archived';
+        toggleArchived.setAttribute('type', 'button');
+        toggleArchived.classList.add('toggle');
+        toggleArchived.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_forums_hideArchived']) toggleArchived.click();
+
+        const toggleIgnored = document.createElement('input');
+        toggleIgnored.id = 'toggle-ignored';
+        toggleIgnored.setAttribute('type', 'button');
+        toggleIgnored.classList.add('toggle');
+        toggleIgnored.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_forums_hideIgnored']) toggleIgnored.click();
+
+      form.appendChild(input);
+      form.appendChild(toggleRead);
+      form.appendChild(toggleClosed);
+      form.appendChild(toggleArchived);
+      form.appendChild(toggleIgnored);
+
+      return form;
+    })();
+
+    // Append form next to search input field
+    const container = document.querySelector('.forum-actions form');
+    if (container) {
+      container.parentNode.classList.add('forum-filters');
+      container.parentNode.appendChild(filterForm);
+    }
+
   });
-}
+
+})();
