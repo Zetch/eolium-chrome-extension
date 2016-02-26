e4dc3f92aaba3da3f2423ccf023d5ba8470557a1
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2016-02-26 14:52:32 +0000

    Code rewritten, simplified. Now ignored threads can be deleted from options page

--- js/options.js
+++ js/options.js
@@ -1,94 +1,101 @@
+(function(){
+  'use strict';
 
-var preferences = {
-  // Header
-  'eolium_header_compactMode':       false,
+  const defaults = {
 
-  // News
-  'eolium_news_singleRow':           false,
+    // Design
+    'eolium_header_compactMode':       false,
 
-  // User Panel
-  'eolium_userZone_compactMode':     true,
+    // Forums
+    'eolium_forums_hideFilter':        '',
+    'eolium_forums_hideRead':          false,
+    'eolium_forums_hideClosed':        false,
+    'eolium_forums_hideArchived':      false,
+    'eolium_forums_hideIgnored':       false,
+    'eolium_forums_ignoredThreads':    [],
 
-  // Forums
-  'eolium_forums_hideRead':          false,
-  'eolium_forums_hideClosed':        false,
-  'eolium_forums_hideArchived':      false,
-  'eolium_forums_hideIgnored':       false,
-  'eolium_forums_ignoredThreads':    {},
+    // Threads
+    'eolium_threads_compactMode':      false,
+    'eolium_threads_hideImages':       false,
 
-  // Threads
-  'eolium_threads_compactMode':      false,
-  'eolium_threads_hideImages':       false,
+    // Other
+    'eolium_other_hideRelatedWikis':   false,
+    'eolium_other_hideGlobalAnnouces': false,
+    'eolium_other_hideRules':          false,
+    'eolium_other_hideBanners':        false
 
-  // Other
-  'eolium_other_hideForumTitle':     false,
-  'eolium_other_hideRelatedWikis':   false,
-  'eolium_other_hideGlobalAnnouces': false,
-  'eolium_other_hideRules':          false
-};
+  };
 
+  chrome.storage.local.get(defaults, (preferences) => {
 
-function toggleThreadsContainer(e) {
-  e.preventDefault();
-  var threads = document.getElementById('eolium_forums_ignoredThreads');
-  threads.classList.toggle('hide');
-  event.target.classList.toggle('open');
-}
-
-
-function savePreferences(e) {
-  var toSave = {};
-  // Load values from form
-  for (name in preferences) {
-    if (name == 'eolium_forums_ignoredThreads') continue;
-    var input = document.getElementById(name);
-    if (input) {
-      var value;
-      if (input.type === "checkbox") {
-        value = input.checked;
-      } else {
-        value = input.value;
+    // Change option and save
+    function toggleCheckbox(event) {
+      const toggle = event.target;
+      if (toggle.id in preferences) {
+        preferences[toggle.id] = toggle.checked;
+        chrome.storage.local.set(preferences);
       }
-      toSave[name] = value;
     }
-  }
-  // Save them to Chrome Storage
-  chrome.storage.local.set(toSave, function() {
-    var status = document.getElementById('status');
-    status.textContent = 'Saved';
-    status.classList.toggle('show');
-    setTimeout(function() {
-      status.classList.toggle('show');
-    }, 2000);
-  });
-}
 
+    // Load preferences and event handling
+    Object.keys(preferences)
+      .map(id => [id, document.getElementById(id)])
+      .filter(pair => pair[1] !== null)
+      .forEach(pair => {
+        pair[1].checked = preferences[pair[0]];
+        pair[1].addEventListener('click', toggleCheckbox);
+      });
+    ;
 
-function loadPreferences(e) {
-  // Restore preferences, it uses default values if they're not defined
-  chrome.storage.local.get(preferences, function(items) {
+    // Load ignored threads
+    const ignored = preferences['eolium_forums_ignoredThreads'];
+    const ignoredTopics = document.getElementById('eolium_forums_ignoredThreads');
 
-    for (name in items) {
-      var input = document.getElementById(name);
-      if (name !== 'eolium_forums_ignoredThreads') {
-        if (input.type === "checkbox") {
-          input.checked = items[name];
-        } else {
-          input.value = items[name];
+    // Show/Hide list toggle
+    document.getElementById('toggleIgnored').addEventListener('click', (event) => {
+      event.preventDefault();
+      ignoredTopics.classList.toggle('hide');
+      event.target.classList.toggle('open');
+    });
+
+    // Remove ignored topic
+    function toggleIgnored(event) {
+      event.preventDefault();
+      const id = parseInt(event.target.id);
+      chrome.storage.local.set({
+        'eolium_forums_ignoredThreads': ignored.filter(topic => topic.id !== id)
+      }, () => {
+        event.target.parentNode.remove();
+      });
+    }
+
+    // Build tree
+    ignored
+      .sort((a, b) => a.forumName > b.forumName)
+      .reduce((prev, next) => {
+        if (prev === null || prev.forumId !== next.forumId) {
+          const span = document.createElement('span');
+          span.classList.add('forum-name');
+          span.textContent = next.forumName;
+          ignoredTopics.appendChild(span);
         }
-      } else {
-        var manager = new IgnoredManager(items['eolium_forums_ignoredThreads']);
-        var tree = manager.buildHtml();
-        input.appendChild(tree);
-      }
-    }
+        const topic = document.createElement('li');
+        const link = document.createElement('a');
+        link.classList.add('topic');
+        link.textContent = next.title;
+        link.setAttribute('href', 'http://www.elotrolado.net/' + next.url);
+        topic.appendChild(link);
+        const toggle = document.createElement('a');
+        toggle.classList.add('toggle');
+        toggle.id = next.id;
+        toggle.textContent = '×';
+        toggle.setAttribute('href', '#')
+        toggle.addEventListener('click', toggleIgnored);
+        topic.appendChild(toggle);
+        ignoredTopics.appendChild(topic);
+        return next;
+      }, null);
+
   });
-}
 
-var toggleIgnored = document.getElementById('toggleIgnored');
-toggleIgnored.addEventListener('click', toggleThreadsContainer);
-
-// Load preferences on load
-document.addEventListener('DOMContentLoaded', loadPreferences);
-// Save preferences on 'Save' link
-document.getElementById('savePreferences').addEventListener('click', savePreferences);
+})();
