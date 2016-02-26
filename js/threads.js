709b932ad06db4363f18e0bd984ad4c3a5f308a9
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2016-02-26 14:51:13 +0000

    Old code removed, now filters keep state between requests

--- js/threads.js
+++ js/threads.js
@@ -1,107 +1,111 @@
-
-function setFilter(keyword, posts) {
-  // Convert text before search
-  var keywd = sanitizeText(keyword);
-
-  // Check rows
-  Array.prototype.forEach.call(posts, function(post) {
-    var content = post.querySelector('.inner .postbody .content');
-    // Check content exists
-    if (content) {
-      var text;
-      try {
-        text = sanitizeText(content.textContent);
-      } catch(err) {
-        text = "";
-      }
-      // Check link text doesn't contain keyword, then hide row
-      if (text.search(keywd) < 0) {
-        // Avoid to add the same class twice or more
-        if (!post.classList.contains('noDisplay')) {
-          post.classList.add('noDisplay');
-        }
-      // If keyword exists but row was previously hidden, show it
-      } else if (post.classList.contains('noDisplay')) {
-        post.classList.remove('noDisplay');
-      // Keyword exists and it's not hidden
-      } else {
-        // OK
-      }
-    }
-  });
-}
-
-// If it's a thread and it can be filtered/toggled
-if (document.querySelector('#content-wrap #forum-wrap') &&
-    document.querySelector('#forum-wrap .topic-actions')) {
-
-  // Bind elements
-  var form    = document.querySelector('#forum-wrap .topic-actions #topic-search');
-  var wrapper = document.querySelector('#forum-wrap');
-  var posts   = document.querySelectorAll('#forum-wrap .post');
-
-  // Create new elements for filtering
-  var inputFilter = document.createElement('input');
-  inputFilter.setAttribute('type',        'text');
-  inputFilter.setAttribute('id',          'inputFilter');
-  inputFilter.setAttribute('placeholder', 'Filtrar mensajes...');
-
-  var buttonCompactMode = document.createElement('button');
-  buttonCompactMode.setAttribute('id',    'toggleCompact');
-  buttonCompactMode.setAttribute('title', 'Modo compacto');
-
-  var buttonImages = document.createElement('button');
-  buttonImages.setAttribute('id',    'toggleImages');
-  buttonImages.setAttribute('title', 'Imágenes');
-  buttonImages.setAttribute('class', 'active');
-
-
-  // Add input filter event, append to page
-  inputFilter.addEventListener('input', function(e) {
-    setFilter(e.target.value, posts);
-  });
-  inputFilter.addEventListener('keydown', function(e) {
-    if (e.keyCode == 27) {
-      e.target.value = '';
-      setFilter(e.target.value, posts);
-    }
-  });
-
-  form.appendChild(inputFilter);
-
+(function(){
+  'use strict';
 
   // Load preferences for toggles
   chrome.storage.local.get({
-    'eolium_threads_compactMode':  false,
-    'eolium_threads_hideImages':   false
+    'eolium_threads_hideImages':  false,
+    'eolium_threads_compactMode': false
 
-  }, function(items) {
-    var preferences = items;
+  }, (preferences) => {
 
-    // Set compact mode by default if it's enabled
-    if (preferences['eolium_threads_compactMode']) {
-      buttonCompactMode.classList.toggle('active');
-      wrapper.classList.toggle('compact');
+    // Get post list
+    const posts = Array.prototype.slice.call(document.querySelectorAll('.row.post'));
+
+    // Sanitize text before filtering
+    function sanitizeText(text) {
+      return text.toLowerCase()
+        .replace(/[àáâãä]/g, "a")
+        .replace(/[èéêẽë]/g, "e")
+        .replace(/[ìíîĩï]/g, "i")
+        .replace(/[òóôõö]/g, "o")
+        .replace(/[ùúûũü]/g, "u");
     }
-    buttonCompactMode.addEventListener('click', function(e) {
-      e.preventDefault();
-      e.target.classList.toggle('active');
-      wrapper.classList.toggle('compact');
-    });
 
-    // Hide images by default if it's enabled
-    if (preferences['eolium_threads_hideImages']) {
-      buttonImages.classList.toggle('active');
-      wrapper.classList.toggle('hideImages');
+    // Set filter by pattern
+    function setFilter(event) {
+      const pattern = sanitizeText(event.target.value);
+
+      posts.forEach((post) => {
+        const content = post.querySelector('.message');
+
+        // If no message (deleted messages)
+        if (!content) return;
+
+        const message = sanitizeText(content.textContent);
+
+        // Check link text doesn't contain keyword, then hide row
+        if (message.search(pattern) < 0) {
+          // Avoid to add the same class twice or more
+          if (!post.classList.contains('hide')) {
+            post.classList.add('hide');
+          }
+        // If keyword exists but post was previously hidden, show it
+        } else if (post.classList.contains('hide')) {
+          post.classList.remove('hide');
+        }
+      });
     }
-    buttonImages.addEventListener('click', function(e) {
-      e.preventDefault();
-      e.target.classList.toggle('active');
-      wrapper.classList.toggle('hideImages');
-    });
 
-    // Append to page
-    form.appendChild(buttonCompactMode);
-    form.appendChild(buttonImages);
+    // Toggle images or compact mode
+    function toggleVisibility(event) {
+      const postList = document.querySelector('.row.post').parentNode;
+      let state;
+
+      if (event.target.id === 'toggle-images') {
+        postList.classList.toggle('hide-images');
+        state = event.target.classList.toggle('active');
+        chrome.storage.local.set({ 'eolium_threads_hideImages': state });
+
+      } else if (event.target.id === 'toggle-compact') {
+        postList.classList.toggle('compact');
+        state = event.target.classList.toggle('active');
+        chrome.storage.local.set({ 'eolium_threads_compactMode': state });
+      }
+    }
+
+    // Create filter form
+    const filterForm = (() => {
+      const form = document.createElement('form');
+
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.setAttribute('placeholder', 'Filtrar mensajes...');
+        input.addEventListener('input', setFilter);
+        input.addEventListener('keydown', (e) => {
+          // Clean filter with 'ESC' key
+          if (e.keyCode === 27) {
+            input.value = '';
+            setFilter(e);
+          }
+        });
+
+        const toggleCompact = document.createElement('input');
+        toggleCompact.id = 'toggle-compact';
+        toggleCompact.setAttribute('type', 'button');
+        toggleCompact.setAttribute('title', 'Modo compacto');
+        toggleCompact.classList.add('toggle');
+        toggleCompact.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_threads_compactMode']) toggleCompact.click();
+
+        const toggleImages = document.createElement('input');
+        toggleImages.id = 'toggle-images';
+        toggleImages.setAttribute('type', 'button');
+        toggleImages.setAttribute('title', 'Ocultar imágenes');
+        toggleImages.classList.add('toggle');
+        toggleImages.addEventListener('click', toggleVisibility);
+        if (preferences['eolium_threads_hideImages']) toggleImages.click();
+
+      form.appendChild(input);
+      form.appendChild(toggleCompact);
+      form.appendChild(toggleImages);
+
+      return form;
+    })();
+
+    const container = document.querySelector('.topic-actions form').parentNode;
+    container.classList.add('topic-filters');
+    container.appendChild(filterForm);
+
   });
-}
+
+})();
